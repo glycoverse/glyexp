6544179052a212eb81922a596ccd4a2e5e5496e1
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2024-10-11 12:43:46 +0800

    feat: add data getters functions

---
+++ R/data-getters.R
@@ -0,0 +1,37 @@
+#' Get the Expression Matrix of an [experiment()]
+#'
+#' A `matrix` of expression values with samples as columns and variables as rows.
+#'
+#' @param exp An [experiment()].
+#' @returns A matrix of expression values.
+#' @export
+get_expr_mat <- function(exp) {
+  stopifnot(class(exp) == "glyexp_experiment")
+  exp$expr_mat
+}
+
+
+#' Get the Sample Information of an [experiment()]
+#'
+#' A `tibble` of sample information, with the first column being "sample".
+#'
+#' @param exp An [experiment()].
+#' @returns A tibble of sample information.
+#' @export
+get_sample_info <- function(exp) {
+  stopifnot(class(exp) == "glyexp_experiment")
+  exp$sample_info
+}
+
+
+#' Get the Variable Information of an [experiment()]
+#'
+#' A `tibble` of variable information, with the first column being "variable".
+#'
+#' @param exp An [experiment()].
+#' @returns A tibble of variable information.
+#' @export
+get_var_info <- function(exp) {
+  stopifnot(class(exp) == "glyexp_experiment")
+  exp$var_info
+}
---
+++ tests/testthat/test-data-getters.R
@@ -0,0 +1,32 @@
+test_that("getter functions work", {
+  expr_mat <- create_expr_mat(c("S1", "S2", "S3"), c("V1", "V2", "V3"))
+  sample_info <- create_sample_info(c("S1", "S2", "S3"))
+  var_info <- create_var_info(c("V1", "V2", "V3"))
+
+  exp <- new_experiment("my_exp", expr_mat, sample_info, var_info)
+
+  expect_equal(get_expr_mat(exp), expr_mat)
+  expect_equal(get_sample_info(exp), sample_info)
+  expect_equal(get_var_info(exp), var_info)
+})
+
+
+test_that("motifying data returned by getters doesn't affect exp", {
+  expr_mat <- create_expr_mat(c("S1", "S2", "S3"), c("V1", "V2", "V3"))
+  sample_info <- create_sample_info(c("S1", "S2", "S3"))
+  var_info <- create_var_info(c("V1", "V2", "V3"))
+
+  exp <- new_experiment("my_exp", expr_mat, sample_info, var_info)
+
+  expr_mat2 <- get_expr_mat(exp)
+  sample_info2 <- get_sample_info(exp)
+  var_info2 <- get_var_info(exp)
+
+  expr_mat2[1, 1] <- 100
+  sample_info2[1, 1] <- "S100"
+  var_info2[1, 1] <- "V100"
+
+  expect_equal(get_expr_mat(exp), expr_mat)
+  expect_equal(get_sample_info(exp), sample_info)
+  expect_equal(get_var_info(exp), var_info)
+})
