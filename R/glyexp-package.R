226974c4ecf875be37d6079894eecd06357a9882
fubin1999 <65430559+fubin1999@users.noreply.github.com>
2026-07-27 01:23:45 +0800

    feat: re-export some SummarizedExperiment functions

--- R/glyexp-package.R
+++ R/glyexp-package.R
@@ -13,6 +13,38 @@
 ## usethis namespace: end
 NULL
 
+#' @export
+#' @importFrom SummarizedExperiment assay
+SummarizedExperiment::assay
+
+#' @export
+#' @importFrom SummarizedExperiment `assay<-`
+SummarizedExperiment::`assay<-`
+
+#' @export
+#' @importFrom SummarizedExperiment rowData
+SummarizedExperiment::rowData
+
+#' @export
+#' @importFrom SummarizedExperiment `rowData<-`
+SummarizedExperiment::`rowData<-`
+
+#' @export
+#' @importFrom SummarizedExperiment colData
+SummarizedExperiment::colData
+
+#' @export
+#' @importFrom SummarizedExperiment `colData<-`
+SummarizedExperiment::`colData<-`
+
+#' @export
+#' @importFrom S4Vectors metadata
+S4Vectors::metadata
+
+#' @export
+#' @importFrom S4Vectors `metadata<-`
+S4Vectors::`metadata<-`
+
 ignore_unused_imports <- function() {
   glyrepr::glycan_composition
 }
